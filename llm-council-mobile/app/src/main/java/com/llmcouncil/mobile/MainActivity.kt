2a742d2fdfd3c5ae49a3ed4c01d75fc745e89379
EnigmaThe1 <149937621+EnigmaThe1@users.noreply.github.com>
2026-08-30 18:43:37 +0300

    v4: add Compose activity host

---
+++ llm-council-mobile/app/src/main/java/com/llmcouncil/mobile/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.llmcouncil.mobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.llmcouncil.mobile.ui.CouncilApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val vm: AppViewModel = viewModel()
+            CouncilApp(vm)
+        }
+    }
+}
